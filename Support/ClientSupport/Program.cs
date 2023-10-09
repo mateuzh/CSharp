235ff1e41ce26b18e5e5b7ae9aa03d50708aca66
Mateus Henrique <mateushh213@gmail.com>
2023-10-08 23:52:03 -0300

    commit

--- Support/ClientSupport/Program.cs
+++ Support/ClientSupport/Program.cs
@@ -44,26 +44,30 @@ namespace trabalho1
 			});
 			
 			//cadastrar usuario
-			app.MapPost("/userRegister", (Database Database, User usuario) =>
+			app.MapPost("/userRegister", (Database Database, User user) =>
 			{
-				Database.Users.Add(usuario);
+                if(user.Name == null || user.Email == null)
+                {
+                    return "Nome ou E-mail não podem ser vazios!";
+                }
+				Database.Users.Add(user);
 				Database.SaveChanges();
-				return "Usuario adicionado com sucesso!";
+				return "Usuario adicionado com sucesso!\n" + user.ToString();
 			});
 			
 			//atualizar usuario
-			app.MapPost("/update/{id}", (Database Database, User usuarioAtualizado, int id) =>
+			app.MapPost("/updateUser/{id}", (Database Database, User updatedUser, int id) =>
 			{
 				var user = Database.Users.Find(id);
-				user.Name = usuarioAtualizado.Name;
-				user.Email = usuarioAtualizado.Email;
-                user.Cellphone = usuarioAtualizado.Cellphone;
+				user!.Name = updatedUser.Name;
+				user.Email = updatedUser.Email;
+                user.Cellphone = updatedUser.Cellphone;
 				Database.SaveChanges();
 				return "Usuario atualizado com sucesso!";
 			});
 						
 			//deletar usuario
-			app.MapPost("/delete/{id}", (Database Database, int id) =>
+			app.MapPost("/deleteUser/{id}", (Database Database, int id) =>
 			{
 				var usuario = Database.Users.Find(id);
 				Database.Remove(usuario);
@@ -74,18 +78,28 @@ namespace trabalho1
             //---------------------- CRUD DO AGENTE DE SUPORTE
 
             //listar todos os agentes de suporte
-			app.MapGet("/agents", (Database Database) => {
+			app.MapGet("/agents", (Database Database) => 
+            {
 				return Database.Agents.ToList();
 			});
 			
 			//listar agente especifico (por id)
-			app.MapGet("/agent/{id}", (Database Database, int id) => {
+			app.MapGet("/agent/{id}", (Database Database, int id) => 
+            {
 				return Database.Agents.Find(id);
 			});
 			
 			//cadastrar agente de suporte
 			app.MapPost("/agentRegister", (Database Database, SupportAgent agent) =>
 			{
+                if(agent.Name == null)
+                {
+                    return "Nome do agente não pode ser vazio!";
+                }
+                if(agent.Email == null)
+                {
+                    return "E-mail não pode ser vazio!";
+                }
 				Database.Agents.Add(agent);
 				Database.SaveChanges();
 				return "Agente adicionado com sucesso!";
@@ -94,6 +108,10 @@ namespace trabalho1
 			//atualizar usuario
 			app.MapPost("/update/{id}", (Database Database, SupportAgent updatedAgent, int id) =>
 			{
+                if(updatedAgent.Id.Equals(false) || updatedAgent.Name == null || updatedAgent.Email == null || updatedAgent.Cellphone.Equals(false))
+                {
+                    return "Os campos não podem estar vazios!";
+                }
 				var agent = Database.Agents.Find(id);
 				agent.Name = updatedAgent.Name;
 				agent.Email = updatedAgent.Email;
@@ -105,6 +123,10 @@ namespace trabalho1
 			//deletar usuario
 			app.MapPost("/delete/{id}", (Database Database, int id) =>
 			{
+                if(Database.Agents.Find(id) == null)
+                {
+                    return "Agente não localizado!";
+                }
 				var agent = Database.Agents.Find(id);
 				Database.Remove(agent);
 				Database.SaveChanges();
@@ -117,9 +139,25 @@ namespace trabalho1
 			app.MapGet("/tickets", (Database Database) => {
 				return Database.ServicesRequest.ToList();
 			});
+
+            //listar todos os tickets
+			app.MapGet("/closedTickets", (Database Database) => 
+            {
+                var tickets = Database.ServicesRequest.ToList();
+                var closedTickets = new List<ServiceRequest>();
+                foreach (var t in tickets)
+                {
+                    if(t.CloseTime.Year != 0001)
+                    {
+                        closedTickets.Add(t);
+                    }
+                }
+                return closedTickets;
+			});
 			
 			//listar ticket especifico (por id)
-			app.MapGet("/ticket/{id}", (Database Database, int id) => {
+			app.MapGet("/ticket/{id}", (Database Database, int id) => 
+            {
 				return Database.ServicesRequest.Find(id);
 			});
 			
@@ -134,17 +172,19 @@ namespace trabalho1
                 {
                     return "Agente não localizado para atribuir chamado!";
                 }
-
                 serviceRequest.OpeningTime = DateTime.Now;
-
 				Database.ServicesRequest.Add(serviceRequest);
 				Database.SaveChanges();
 				return "Chamado aberto com sucesso! Número: " + serviceRequest.Id;
 			});
 			
 			//atualizar agente do chamado
-			app.MapPost("/update/{id}", (Database Database, ServiceRequest updatedTask, int id) =>
+			app.MapPost("/reatribuirChamado/{id}", (Database Database, ServiceRequest updatedTask, int id) =>
 			{
+                if(updatedTask.AgentId == 0 || updatedTask.AgentId == null)
+                {
+                    return "Não é possível alterar o agente para vazio.";
+                }
 				var task = Database.ServicesRequest.Find(id);
 				task.AgentId = updatedTask.AgentId;
 				Database.SaveChanges();
@@ -154,7 +194,10 @@ namespace trabalho1
 			//encerrar chamado
 			app.MapPost("/closeTicket/{id}", (Database Database, int id) =>
 			{
-
+                if(Database.ServicesRequest.Find(id) == null)
+                {
+                    return "Chamado não localizado!";
+                }
 				var serviceRequest = Database.ServicesRequest.Find(id);
 				serviceRequest.CloseTime = DateTime.Now;
 				Database.SaveChanges();
--- Support/ClientSupport/ServiceRequest.cs
+++ Support/ClientSupport/ServiceRequest.cs
@@ -5,6 +5,7 @@ class ServiceRequest
     public int AgentId {get; set;}
     public DateTime OpeningTime {get; set;}
     public DateTime CloseTime {get; set;}
+    
     public ServiceRequest(int id, int userId, int agentId, DateTime openingTime)
     {
         Id = id;
